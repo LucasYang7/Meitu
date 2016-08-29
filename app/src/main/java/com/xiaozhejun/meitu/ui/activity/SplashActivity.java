df65bcafc4297aaf1c927ff8ad0b6594613af594
LucasYang7 <731180001@qq.com>
2016-08-29 12:37:28 +0800

    change the typeface of textview in SplashActivity

--- app/src/main/java/com/xiaozhejun/meitu/ui/activity/SplashActivity.java
+++ app/src/main/java/com/xiaozhejun/meitu/ui/activity/SplashActivity.java
@@ -1,14 +1,18 @@
 package com.xiaozhejun.meitu.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.xiaozhejun.meitu.R;
 import com.xiaozhejun.meitu.util.task.InitRetrofitClientTask;
+
+import org.w3c.dom.Text;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -21,6 +25,12 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // 加载布局
         setContentView(R.layout.activity_splash);
+
+        // 设置字体的样式
+        TextView textView = (TextView)findViewById(R.id.textViewInSplash);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/android_7.ttf");
+        textView.setTypeface(typeface);
+
         // 初始化Retrofit客户端
         new InitRetrofitClientTask().execute();
         // 延迟3秒后进入MainActivity
